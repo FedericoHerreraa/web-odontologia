e3ef015e2de4a4c037369e9835edd6aad6293606
federicoherrera <fede.juan.herrera@gmail.com>
2024-05-14 11:41:46 -0300

    adelantos de diseño

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,27 +7,38 @@ import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.headerSuperior}>
-        <div className={styles.telefono}>
-          <LocalPhoneIcon/>
-          <p>+54 (911) 6296-4493</p>
+    <div className={styles.contenedor}>
+      <header className={styles.header}>
+        <div className={styles.headerSuperior}>
+          <div className={styles.telefono}>
+            <LocalPhoneIcon/>
+            <p>+54 (911) 6296-4493</p>
+          </div>
+          <div className={styles.nombre}>
+            <p>Christian Saad</p>
+          </div>
+          <div className={styles.redes}>
+            <WhatsAppIcon fontSize='large'/>
+            <MailOutlineIcon fontSize='large'/>
+            <InstagramIcon fontSize='large'/>
+          </div>
         </div>
-        <div className={styles.nombre}>
-          <p>Christian Saad</p>
+        <div className={styles.headerInferior}>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>Inicio</Link>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/nuestrosServicios'>Nuestros servicios</Link>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/sobreNosotros'>Sobre nosotros</Link>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/contactanos'>Contactanos</Link>
         </div>
-        <div className={styles.redes}>
-          <WhatsAppIcon fontSize='large'/>
-          <MailOutlineIcon fontSize='large'/>
-          <InstagramIcon fontSize='large'/>
+      </header>
+      <div className={styles.infoImg}>
+        <h1>Dr. Christian Saad</h1>
+        <div>
+          <h2>Doctor of Dental Medicine</h2>
+          <p>Dr. Shannons has over 10 years of experience in dentistry. He is committed to providing personalized care to his clients, and keeping his.</p>
         </div>
       </div>
-      <div className={styles.headerInferior}>
-        <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>Inicio</Link>
-        <Link style={{ textDecoration: 'none', color: 'white' }} to='/nuestrosServicios'>Nuestros servicios</Link>
-        <Link style={{ textDecoration: 'none', color: 'white' }} to='/sobreNosotros'>Sobre nosotros</Link>
-        <Link style={{ textDecoration: 'none', color: 'white' }} to='/contactanos'>Contactanos</Link>
-      </div>
-    </header>
+    </div>
+
+
   )
 }
