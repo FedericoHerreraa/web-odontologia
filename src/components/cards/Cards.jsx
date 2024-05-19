15f840f2192fbf31d7be15004c89d437cf5c0bb0
CamilaAznar <aznarcami@gmail.com>
2024-05-19 17:00:14 -0300

    cambios en contacto

---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,15 @@
+import {style} from "./Cards.module.css"
+
+export const Cards = () => {
+  return (
+    <div className={style.contenedor}>
+        <div>
+            <div>
+                <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo tenetur quo iure eos quis tempora, doloribus unde laudantium perferendis minus tempore nobis cumque, autem, cum repudiandae a laboriosam praesentium dignissimos?</div>
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet recusandae aliquid ducimus ullam et at dolor itaque excepturi, saepe, aut numquam alias vel accusantium quibusdam quia sapiente cum dolores autem.</div>
+                <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet praesentium rem placeat sit, fuga dolore nemo. Deserunt natus laborum saepe fuga. Optio est consectetur iusto ipsa impedit laudantium vero animi.</div>
+            </div>
+        </div>
+    </div>
+  )
+}
