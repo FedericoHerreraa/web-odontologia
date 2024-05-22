f8d58139d50ebd53c37a1f25bf3c3f12ebbaa3c7
federicoherrera <fede.juan.herrera@gmail.com>
2024-05-22 12:45:06 -0300

    nueva animacion

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,10 @@
 import Description from "../../components/description/Description";
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// import Slider from "react-slick";
 
 export default function Home() {
   return (
@@ -7,16 +12,18 @@ export default function Home() {
       <div className={styles.infoImg}>
         <h1>Dr. Christian Saad</h1>
         <p className={styles.descripcion}>
-          Consultorio de odontologia segura, confiable y la mejor de la zona.
+          Consultorio de odontología seguro, confiable y el mejor de la zona.
         </p>
-        <div className={styles.especialidades}>
-          <p>Odontología General</p>
-          <p>Ortodoncia</p>
-          <p>Implantología</p>
-          <p>Endodoncia</p>
-          <p>Periodoncia</p>
-          <p>Cirugía Oral y Maxilofacial</p>
-          <p>Estética Dental</p>
+        <div className={styles.containerSlider}>
+          <div className={styles.especialidades}>
+            <p>Odontología General</p>
+            <p>Ortodoncia</p>
+            <p>Implantología</p>
+            <p>Endodoncia</p>
+            <p>Periodoncia</p>
+            <p>Cirugía Oral y Maxilofacial</p>
+            <p>Estética Dental</p>
+          </div>
         </div>
       </div>
       <Description />
