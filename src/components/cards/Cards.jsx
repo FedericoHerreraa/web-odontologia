f8a0738c4d71bdebf8f595046532599277384105
CamilaAznar <aznarcami@gmail.com>
2024-05-28 17:35:29 -0300

    Modificacion en componente Card

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,29 +1,49 @@
-import styles from '/src/components/cards/Cards.module.css'
+import styles from "/src/components/cards/Cards.module.css";
 import CustomSeparator from "../custom-separator/CustomSeparator";
+import img from "./../../../img/imagen-consultorio.jpg"
+// import img2 from "./../../../img/imagen-tratamiento.jpg"
 
 export const Cards = () => {
   return (
     <div className={styles.contenedorFondo}>
-      <CustomSeparator/>
+      <CustomSeparator />
       <div className={styles.contenedor}>
-      
         <div className={styles.card}>
-          <h3>Conocenos</h3>
-            <p>Conocé nuestro consultorio, donde la salud y la estética dental se unen para brindarte una experiencia única. Nuestro equipo de expertos está comprometido con tu bienestar, utilizando tecnología de punta y tratamientos personalizados para garantizarte los mejores resultados</p>
+          <img src={img} alt="imagenConocenos" />
+          <div>
+            <h3>Conocenos</h3>
+            <p>
+              Conocé nuestro consultorio, donde la salud y la estética dental se
+              unen para brindarte una experiencia única. Nuestro equipo de
+              expertos está comprometido con tu bienestar, utilizando tecnología
+              de punta y tratamientos personalizados para garantizarte los
+              mejores resultados
+            </p>
             <button>Ver más</button>
+          </div>
         </div>
         <div className={styles.card}>
-          <h3>Conocenos</h3>
-          <p>Conocé nuestro consultorio, donde la salud y la estética dental se unen para brindarte una experiencia única. Nuestro equipo de expertos está comprometido con tu bienestar, utilizando tecnología de punta y tratamientos personalizados para garantizarte los mejores resultados</p>
-          <button>Ver más</button>
+          <div>
+            <h3>Tratamientos</h3>
+            <p>
+              En nuestro consultorio, estamos dedicados a cuidar de tu salud
+              bucal con una amplia gama de servicios diseñados para ti y tu
+              familia. Nuestro equipo de profesionales altamente capacitados
+              utiliza la última tecnología para garantizarte una atención de
+              calidad y resultados excepcionales.
+            </p>
+            <button>Ver más</button>
+          </div>
+          <div className={styles.imagen1}>
+            <div>
+              <h2>tratamientos</h2>
+              <p>Ortodoncia</p>
+              <p>Odontologia</p>
+              <p>Blanqueamiento</p>
+            </div>
+          </div>
         </div>
-        <div className={styles.card}>
-          <h3>Tratamientos</h3>
-          <p>En nuestro consultorio, estamos dedicados a cuidar de tu salud bucal con una amplia gama de servicios diseñados para ti y tu familia. Nuestro equipo de profesionales altamente capacitados utiliza la última tecnología para garantizarte una atención de calidad y resultados excepcionales.</p>
-          <button>Ver más</button>
-        </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
