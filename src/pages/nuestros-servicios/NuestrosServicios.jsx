95f708037d37c044a97a408b2d1f7d638fb43251
CamilaAznar <aznarcami@gmail.com>
2024-06-26 11:56:38 -0300

    modals

--- src/pages/nuestros-servicios/NuestrosServicios.jsx
+++ src/pages/nuestros-servicios/NuestrosServicios.jsx
@@ -3,7 +3,14 @@ import styles from './NuestrosServicios.module.css';
 import { FaTeeth, FaTooth, FaTeethOpen } from 'react-icons/fa';
 import CustomSeparator from '@/components/custom-separator/CustomSeparator';
 import ScrollToTop from '@/components/scroll/ScrollToTop';
-import img from "./../../../img/ChristianSaad-02.png"
+import img1 from "./../../../img/modals/blanqueamiento.jpg"
+import img2 from "./../../../img/modals/protesis.jpg"
+import img3 from "./../../../img/modals/ortodoncia.jpg"
+import img4 from "./../../../img/modals/odontologia.jpg"
+import img5 from "./../../../img/modals/endodoncia.jpg"
+import img6 from "./../../../img/modals/periodoncia.jpg"
+
+
 import {
     AlertDialog,
     AlertDialogCancel,
@@ -35,120 +42,119 @@ export default function NuestrosServicios() {
                                     <FaTeeth className={styles.icono} />
                                     <div className={styles.item}>
                                         <h2>BLANQUEAMIENTO DENTAL</h2>
-                                        <p>Tratamientos de blanqueamiento dental interno o externo para obtener una sonrisa más blanca y radiante en poco minutos.</p>
+                                        <p>Tratamientos de blanqueamiento dental interno o externo para obtener una sonrisa más blanca y radiante en pocos minutos.</p>
                                     </div>
                                 </div>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogTitle>Blanqueamiento Dental</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    <img src={img} alt="" />
-                                    Tratamientos de blanqueamiento dental interno o externo para obtener una sonrisa más blanca y radiante en poco minutos.
+                            <AlertDialogContent className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-3xl">
+                                <AlertDialogTitle className="text-2xl font-bold text-customBlue text-center mb-4">Blanqueamiento Dental</AlertDialogTitle>
+                                <AlertDialogDescription className="flex text-lg flex-col sm:flex-row items-center sm:items-start w-full">
+                                    <img src={img1} className='w-full sm:w-1/2 h-auto mb-4 sm:mb-0 sm:mr-4' alt="" />
+                                    Tratamientos de blanqueamiento dental interno o externo para obtener una sonrisa más blanca y radiante en pocos minutos.
                                 </AlertDialogDescription>
-                                <AlertDialogCancel>Salir</AlertDialogCancel>
+                                <AlertDialogCancel className="mt-4 px-6 py-2 bg-customBlue text-white rounded-md">Salir</AlertDialogCancel>
                             </AlertDialogContent>   
                         </AlertDialog>
+
                         <AlertDialog>
                             <AlertDialogTrigger>
                                 <div className={styles.cardContainer}>
                                     <FaTooth className={styles.icono} />
                                     <div className={styles.item}>
-                                        <h2>PRÓTESIS DENTALES</h2>
+                                        <h2 className="text-xl font-semibold">PRÓTESIS DENTALES</h2>
                                         <p>Restauración de piezas dentales a través de Coronas, Puentes, Prótesis o Incrustaciones. Contamos con Fresadora 3D ultra precisa.</p>
                                     </div>
-                                </div>                                
+                                </div>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogTitle>Protésis Dentales</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    <img src={img} alt="" />
+                            <AlertDialogContent className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-3xl">
+                                <AlertDialogTitle className="text-2xl font-bold text-customBlue text-center mb-4">Protésis Dentales</AlertDialogTitle>
+                                <AlertDialogDescription className="flex text-lg flex-col sm:flex-row items-center sm:items-start w-full">
+                                    <img src={img2} className='w-full sm:w-1/2 h-auto mb-4 sm:mb-0 sm:mr-4' alt="" />
                                     Restauración de piezas dentales a través de Coronas, Puentes, Prótesis o Incrustaciones. Contamos con Fresadora 3D ultra precisa.
                                 </AlertDialogDescription>
-                                <AlertDialogCancel>Salir</AlertDialogCancel>
+                                <AlertDialogCancel className="mt-4 px-6 py-2 bg-customBlue text-white rounded-md">Salir</AlertDialogCancel>
                             </AlertDialogContent>
-                            
                         </AlertDialog>
+
                         <AlertDialog>
                             <AlertDialogTrigger>
                                 <div className={styles.cardContainer}>
                                     <FaTeethOpen className={styles.icono} />
                                     <div className={styles.item}>
-                                        <h2>ORTODONCIA</h2>
+                                        <h2 className="text-xl font-semibold">ORTODONCIA</h2>
                                         <p>Alineación dental por medio de Brackets o Placas Alineadoras. Disponemos de sistemas de Ortodoncia Cosmética casi invisibles.</p>
                                     </div>
-                                </div>                            
+                                </div>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogTitle>Ortodoncia</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    <img src={img} alt="" />
+                            <AlertDialogContent className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-3xl">
+                                <AlertDialogTitle className="text-2xl font-bold text-customBlue text-center mb-4">Ortodoncia</AlertDialogTitle>
+                                <AlertDialogDescription className="flex text-lg flex-col sm:flex-row items-center sm:items-start w-full">
+                                    <img src={img3} className='w-full sm:w-1/2 h-auto mb-4 sm:mb-0 sm:mr-4' alt="" />
                                     Alineación dental por medio de Brackets o Placas Alineadoras. Disponemos de sistemas de Ortodoncia Cosmética casi invisibles.
                                 </AlertDialogDescription>
-                                <AlertDialogCancel>Salir</AlertDialogCancel>
+                                <AlertDialogCancel className="mt-4 px-6 py-2 bg-customBlue text-white rounded-md">Salir</AlertDialogCancel>
                             </AlertDialogContent>
-                            
                         </AlertDialog>
+
                         <AlertDialog>
                             <AlertDialogTrigger>
                                 <div className={styles.cardContainer}>
                                     <FaTooth className={styles.icono} />
                                     <div className={styles.item}>
-                                        <h2>ODONTOLOGÍA GENERAL</h2>
+                                        <h2 className="text-xl font-semibold">ODONTOLOGÍA GENERAL</h2>
                                         <p>Diagnóstico y restauración de piezas dentarias afectadas por pequeñas caries o traumatismos leves que no requieren prótesis.</p>
                                     </div>
-                                </div>                           
+                                </div>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogTitle>Odontología General</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    <img src={img} alt="" />
+                            <AlertDialogContent className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-3xl">
+                                <AlertDialogTitle className="text-2xl font-bold text-customBlue text-center mb-4">Odontología General</AlertDialogTitle>
+                                <AlertDialogDescription className="flex text-lg flex-col sm:flex-row items-center sm:items-start w-full">
+                                    <img src={img4} className='w-full sm:w-1/2 h-auto mb-4 sm:mb-0 sm:mr-4' alt="" />
                                     Diagnóstico y restauración de piezas dentarias afectadas por pequeñas caries o traumatismos leves que no requieren prótesis.
                                 </AlertDialogDescription>
-                                <AlertDialogCancel>Salir</AlertDialogCancel>
+                                <AlertDialogCancel className="mt-4 px-6 py-2 bg-customBlue text-white rounded-md">Salir</AlertDialogCancel>
                             </AlertDialogContent>
-                            
                         </AlertDialog>
+
                         <AlertDialog>
                             <AlertDialogTrigger>
                                 <div className={styles.cardContainer}>
                                     <FaTooth className={styles.icono} />
                                     <div className={styles.item}>
-                                        <h2>ENDODONCIA</h2>
+                                        <h2 className="text-xl font-semibold">ENDODONCIA</h2>
                                         <p>Tratamiento de conducto radicular para la eliminación del nervio causante del dolor, debido a infección por caries, o fractura de la raíz.</p>
                                     </div>
-                                </div>                        
+                                </div>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogTitle>Endodoncia</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    <img src={img} alt="" />
+                            <AlertDialogContent className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-3xl">
+                                <AlertDialogTitle className="text-2xl font-bold text-customBlue text-center mb-4">Endodoncia</AlertDialogTitle>
+                                <AlertDialogDescription className="flex text-lg flex-col sm:flex-row items-center sm:items-start w-full">
+                                    <img src={img5} className='w-full sm:w-1/2 h-auto mb-4 sm:mb-0 sm:mr-4' alt="" />
                                     Tratamiento de conducto radicular para la eliminación del nervio causante del dolor, debido a infección por caries, o fractura de la raíz.
                                 </AlertDialogDescription>
-                                <AlertDialogCancel>Salir</AlertDialogCancel>
+                                <AlertDialogCancel className="mt-4 px-6 py-2 bg-customBlue text-white rounded-md">Salir</AlertDialogCancel>
                             </AlertDialogContent>
-                            
                         </AlertDialog>
+
                         <AlertDialog>
                             <AlertDialogTrigger>
                                 <div className={styles.cardContainer}>
                                     <FaTeeth className={styles.icono} />
                                     <div className={styles.item}>
-                                        <h2>PERIODONCIA</h2>
+                                        <h2 className="text-xl font-semibold">PERIODONCIA</h2>
                                         <p>Tratamiento de enfermedades relacionadas a los tejidos de soporte del diente, como sangrado de encías, sensibilidad y movilidad dental.</p>
                                     </div>
-                                </div>                    
+                                </div>
                             </AlertDialogTrigger>
-                            <AlertDialogContent>
-                                <AlertDialogTitle>Periodoncia</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    <img src={img} alt="" />
+                            <AlertDialogContent className="flex flex-col items-center p-6 bg-white rounded-lg shadow-lg max-w-3xl">
+                                <AlertDialogTitle className="text-2xl font-bold text-customBlue text-center mb-4">Periodoncia</AlertDialogTitle>
+                                <AlertDialogDescription className="flex text-lg flex-col sm:flex-row items-center sm:items-start w-full">
+                                    <img src={img6} className='w-full sm:w-1/2 h-auto mb-4 sm:mb-0 sm:mr-4' alt="" />
                                     Tratamiento de enfermedades relacionadas a los tejidos de soporte del diente, como sangrado de encías, sensibilidad y movilidad dental.
                                 </AlertDialogDescription>
-                                <AlertDialogCancel>Salir</AlertDialogCancel>
+                                <AlertDialogCancel className="mt-4 px-6 py-2 bg-customBlue text-white rounded-md">Salir</AlertDialogCancel>
                             </AlertDialogContent>
-                            
-                        </AlertDialog>
-                        
+                        </AlertDialog>  
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        customBlue: "#001f3f",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
